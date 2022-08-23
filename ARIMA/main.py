3663f15e15dc5543c7f17e53a09d65d50b17e95c
Vivek Sridhar <vivekhsridhar@Viveks-Mac.fritz.box>
2022-08-23 12:37:32 +0200

    with just LR

--- ARIMA/main.py
+++ ARIMA/main.py
@@ -38,28 +38,57 @@ def arima(series, n_pred):
 
     return np.array(predictions)
 
+def linear_regression(series, n_pred):
+    """
+    Generates toy-model ARIMA based predictions for the given time-series.
+    Args:
+        series (array-like): series upon which to do forecasting
+        n_pred (int): number of points used in prediction in every iteration
+    """
+
+    assert len(series) > n_pred, "series is smaller than n_pred."
+    predictions = [[], []]
+
+    for i in range(len(series) - n_pred):
+        training_points = series[i : i+n_pred] #Subset of differences series
+        LM = linregress(x=np.arange(i, i+n_pred), y=training_points) # Regression
+        predictions[0].append(i+n_pred+1)
+        training_points = list(training_points)
+        training_points.append(LM.slope*(i+n_pred+1) + LM.intercept)
+
+        predictions[1].append(training_points[-1])
+
+    return np.array(predictions)
+
 def main():
 
     x1 = signal_creation.generate_signal(0, 500, 1, [0.1, 1], 0.1)
     x2 = signal_creation.combination_of_waves(np.arange(0,4000), 4, np.array([1,1,7,0.4]), np.array([500, 200, 1000, 40]), np.array([-0.78, 0.1, 0.3, 1.57]))
     x3 = pd.read_csv('jena_climate_2009_2016.csv', sep=',', header=0)['T (degC)'].to_numpy()[:10000]
 
-    x1_preds = arima(x1[1], 10)
-    x2_preds = arima(x2[1], 10)
-    x3_preds = arima(x3, 10)
+    x1_arima = arima(x1[1], 10)
+    x2_arima = arima(x2[1], 10)
+    x3_arima = arima(x3, 10)
+
+    x1_linreg = linear_regression(x1[1], 10)
+    x2_linreg = linear_regression(x2[1], 10)
+    x3_linreg = linear_regression(x3, 10)
 
     fig, axs = plt.subplots(3,1, layout='tight')
     axs[0].scatter(x1[0], x1[1], s=SCATTER_SIZE, label="Real data")
-    axs[0].plot(x1_preds[0], x1_preds[1], color="red", label="toy-ARIMA predictions")
+    axs[0].plot(x1_arima[0], x1_arima[1], color="red", label="toy-ARIMA predictions")
+    axs[0].plot(x1_linreg[0], x1_linreg[1], color="yellow", linestyle='dotted', label="linear regression predictions")
     axs[0].legend()
     axs[0].set_title('Linear growth with noise')
 
     axs[1].scatter(x2[0], x2[1], s=SCATTER_SIZE)
-    axs[1].plot(x2_preds[0], x2_preds[1], color="red")
+    axs[1].plot(x2_arima[0], x2_arima[1], color="red")
+    axs[1].plot(x2_linreg[0], x2_linreg[1], linestyle='dotted', color="yellow")
     axs[1].set_title('Combination of random sine waves')
 
     axs[2].scatter(np.arange(0, len(x3)), x3, s=SCATTER_SIZE)
-    axs[2].plot(x3_preds[0], x3_preds[1], color="red")
+    axs[2].plot(x3_arima[0], x3_arima[1], color="red")
+    axs[2].plot(x3_linreg[0], x3_linreg[1], linestyle='dotted', color="yellow")
     axs[2].set_title('Jena Climate Data: Temperature in deg C')
 
 
